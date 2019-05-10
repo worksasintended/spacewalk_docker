333564beba80462d73ac1827861cf897a88638e5
worksasintended <marc@marschalls.net>
2019-05-10 10:40:31 +0200

    it starts

--- spacewalk/init.sh
+++ spacewalk/init.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+sleep 20
 /init_database.sh
 spacewalk-setup --external-postgresql --answer-file=/answer.txt --clear-db --skip-services-restart --non-interactive
+# set permissions on mounted storages
+chown apache.apache -R /var/satellite
+
+# start cobblerd
+/usr/bin/python -s /usr/bin/cobblerd
+
+# start taskomatic
+/usr/sbin/taskomatic start
+
+# start rhn-search
+rhn-search start
+
+# start osa-dispatcher
+/usr/bin/router -c /etc/jabberd/router.xml &
+/usr/bin/sm -c /etc/jabberd/sm.xml &
+/usr/bin/c2s -c /etc/jabberd/c2s.xml &
+/usr/bin/s2s -c /etc/jabberd/s2s.xml &
+
+# start httpd
+[ -f /var/run/httpd/httpd.pid ] && rm /var/run/httpd/httpd.pid
+httpd -k start
+
+# start tomcat
+/usr/libexec/tomcat/server start
